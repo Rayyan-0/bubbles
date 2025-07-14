0789bd60fe81712b0ed55df7a4dd917f694d0aed
rayyan-0 <rayyanalmalki1@gmail.com>
2025-07-14 17:54:20 +0300

    Finished

--- dist/script.js
+++ dist/script.js
@@ -1,31 +1,29 @@
 "use strict";
-const array = [8, 4, 2];
-const notSorted = false;
-const sorted = true;
-const bubbleSort = (arr) => {
-    let index1 = 0;
-    let index2 = 1;
-    let placeHolder = 0;
-    while (true) {
-        if (index1 > index2) {
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[index1] > arr[index2]) {
-                    placeHolder = arr[index1];
-                    arr[index1] = arr[index2];
-                    arr[index2] = placeHolder;
-                    index1 += 1;
-                    index2 += 2;
-                }
-            }
-        }
-        else if (index2 > arr.length) {
-            index1 += 1;
-            index2 += 1;
-        }
-        else {
-            break;
+const array = [8, 4, 2, 3];
+let isSorted = true;
+const isComplete = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return isSorted = false;
         }
     }
+    return isSorted = true;
+};
+const bubbleSort = (arr) => {
+    do {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                isComplete(arr);
+            }
+            else {
+                continue;
+            }
+        }
+    } while (isSorted == false);
     return arr;
 };
+console.log(bubbleSort(array));
 //# sourceMappingURL=script.js.map
